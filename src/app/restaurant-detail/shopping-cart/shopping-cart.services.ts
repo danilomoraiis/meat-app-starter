050a0777515dc43a2e49149f9cb9f99687f10c4d
Danilo Morais <danilo.morais@yaman.com.br>
2018-04-30 11:17:26 -0300

    initial files

---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.services.ts
@@ -0,0 +1,53 @@
+import { CartItem } from "./cart-item.model";
+import { MenuItem } from "../menu-item/menu-item.model";
+import { Injectable } from "@angular/core";
+import { NotificationService } from "../../shared/messages/notification.service";
+
+@Injectable()
+export class ShoppingCartService {
+    items: CartItem[] = []
+
+    constructor(private notificationService: NotificationService){}
+
+    clear(){
+        this.items = []
+    }
+
+    addItem(item:MenuItem){
+        //verifica se o id do produto que esta no menu
+        //é igual ao id que sera add no carrinho
+        let foundItem = this.items.find((mItem) => mItem.menuItem.id == item.id)
+        if(foundItem)
+        {
+            this.increaseQty(foundItem)
+        } else 
+        {
+            this.items.push(new CartItem(item))
+        }
+        this.notificationService.notify(`Você adicionou o item ${item.name}`)
+    }
+
+    increaseQty(item: CartItem){
+        item.quantity = item.quantity + 1
+    }
+
+    decreaseQty(item: CartItem){
+        item.quantity = item.quantity - 1
+
+        if(item.quantity === 0)
+        {
+            this.removeItem(item)
+        }
+    }
+
+    removeItem(item:CartItem){
+        this.items.splice(this.items.indexOf(item), 1)
+        this.notificationService.notify(`Você adicionou o item ${item.menuItem.name}`)
+    }
+
+    total(): number{
+        return this.items
+        .map(item => item.value())
+        .reduce((prev, value) => prev + value, 0)
+    }
+}
